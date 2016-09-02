f972227f903d8e28b970865b18c0d47e1739ded9
Cal Stephens <cal@calstephens.tech>
2016-09-02 12:12:24 -0400

    Style changes to view controller. Specifically, use handler: nil instead of empty closures.

--- Guess The Number/ViewController.swift
+++ Guess The Number/ViewController.swift
@@ -18,21 +18,21 @@ class ViewController: UIViewController {
     var numberToGuess: Int!
     var numberOfGuesses = 0
     
+    
+    //MARK - View Setup
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         generateRandomNumber()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
     func generateRandomNumber() {
         numberToGuess = Int(arc4random_uniform(100)) + 1
-        //print(numberToGuess)
     }
 
+    
+    //MARK: - User Interaction
+    
     @IBAction func submitButtonPressed(sender: UIButton) {
         if let guess = guessTextField.text {
             if let guessInt = Int(guess) {
@@ -59,18 +59,25 @@ class ViewController: UIViewController {
         guessTextField.text = ""
     }
     
+    
+    //MARK: - Alerts
+    
     func showBoundsAlert() {
         let alert = UIAlertController(title: "Hey!", message: "Your guess should be between 1 and 100!", preferredStyle: .Alert)
-        let action = UIAlertAction(title: "Got it", style: .Default) { _ in }
+        
+        let action = UIAlertAction(title: "Got it", style: .Default, handler: nil)
         alert.addAction(action)
-        self.presentViewController(alert, animated: true) {}
+        
+        self.presentViewController(alert, animated: true, completion: nil)
     }
     
     func showWinAlert() {
         let alert = UIAlertController(title: "Congrats! 🎉", message: "You won with a total of \(numberOfGuesses) guesses", preferredStyle: .Alert)
-        let action = UIAlertAction(title: "Play again", style: .Default) { _ in }
+        
+        let action = UIAlertAction(title: "Play again", style: .Default, handler: nil)
         alert.addAction(action)
-        self.presentViewController(alert, animated: true) {}
+        
+        self.presentViewController(alert, animated: true, completion: nil)
     }
     
 }
